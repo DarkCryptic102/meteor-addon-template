85a377d63e66ba277af2bf706b52b594c7a0ce9b
DarkCryptic102 <demolishedcryptic@gmail.com>
2026-04-23 07:57:06 +0530

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -8,27 +8,25 @@ import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
+
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    public static final Category CATEGORY = new Category("Dyselexia");
+    public static final meteordevelopment.meteorclient.systems.hud.HudGroup HUD_GROUP =
+        new meteordevelopment.meteorclient.systems.hud.HudGroup("Dyselexia");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Dyselexia Addon");
 
-        // Modules
         Modules.get().add(new ModuleExample());
-
-        // Commands
         Commands.add(new CommandExample());
 
-        // HUD
         Hud.get().register(HudExample.INFO);
     }
 
